e3eac0698c7ecfa005b2d2c8c8aa2edcfc61d595
MatheusPalmieri <matheuspalmieridev@gmail.com>
2024-05-08 00:20:35 -0300

    feat: initial

--- src/main.tsx
+++ src/main.tsx
@@ -6,19 +6,12 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 
-// Mantine
-import { MantineProvider } from '@mantine/core';
-import { theme } from './styles/theme';
-
 // Styles
-import '@mantine/core/styles.css';
-import './styles/reset.css';
+import './styles/tailwind.css';
 import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <MantineProvider theme={theme}>
-      <RouterProvider router={router} />
-    </MantineProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,8 @@
-import { Container, Flex, Text } from '@mantine/core';
-
 export const Home = () => (
-  <Container>
-    <Flex h='100dvh' direction='column' align='center' justify='center'>
-      <Text fz='h1' fw='bold'>
-        Boilerplate React{' '}
-        <Text span fz='h1' c='green.6'>
-          0.0.1
-        </Text>
-      </Text>
-    </Flex>
-  </Container>
+  <section className='container'>
+    <h1>Template React</h1>
+    <p>
+      Created by <span>Matheus Palmieri</span>
+    </p>
+  </section>
 );
